66ec5ddec4b32623c2d4b26e1587612129740b9b
zhengzhuang95 <zheng951203@163.com>
2021-07-08 15:54:09 +0800

    spark-ui

--- examples/main.js
+++ examples/main.js
@@ -2,15 +2,15 @@
  * @Author: zhengzhuang
  * @Date: 2021-07-07 17:35:31
  * @LastEditors: zhengzhuang
- * @LastEditTime: 2021-07-07 17:44:26
+ * @LastEditTime: 2021-07-08 11:48:39
  * @Description: In User Settings Edit
- * @FilePath: /rain-ui/examples/main.js
+ * @FilePath: /spark-ui/examples/main.js
  */
 import {
   createApp
 } from 'vue'
 import App from './App.vue'
 
-import Rain from './../packages'
+import SpackUI from './../packages'
 
-createApp(App).use(Rain).mount('#app')
+createApp(App).use(SpackUI).mount('#app')
--- packages/spark-button/index.js
+++ packages/spark-button/index.js
@@ -2,11 +2,11 @@
  * @Author: zhengzhuang
  * @Date: 2021-07-08 11:36:46
  * @LastEditors: zhengzhuang
- * @LastEditTime: 2021-07-08 11:37:40
+ * @LastEditTime: 2021-07-08 15:44:57
  * @Description: In User Settings Edit
  * @FilePath: /spark-ui/packages/spark-button/index.js
  */
-import SparkButton from './src/button'
+import SparkButton from './src/button.vue'
 
 /* istanbul ignore next */
 SparkButton.install = function (Vue) {
